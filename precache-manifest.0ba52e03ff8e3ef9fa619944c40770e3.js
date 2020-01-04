7192dd02dbcbd1eca69e24a6968406eaad1e22cc
Tigredepoche <Tigredepoche.lol@gmail.com>
2020-01-04 08:18:27 +0100

    github pages upload

---
+++ precache-manifest.0ba52e03ff8e3ef9fa619944c40770e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3214e3d819f3aad74f42a86a96be95d4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12c41be8eb55181fb70f",
+    "url": "/monsters-rolodex/static/css/main.05d1312b.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c41be8eb55181fb70f",
+    "url": "/monsters-rolodex/static/js/main.118461d4.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
